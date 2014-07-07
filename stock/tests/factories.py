789a19363997bca6cc70ba35b58371e943b9dc94
Patrick Kimber <code@pkimber.net>
2014-07-07 13:49:34 +0100

    tidy

--- stock/tests/factories.py
+++ stock/tests/factories.py
@@ -5,10 +5,6 @@ from django.template.defaultfilters import slugify
 
 import factory
 
-from factory import SubFactory
-from factory.django import DjangoModelFactory
-from factory.fuzzy import FuzzyDecimal
-
 from stock.models import (
     Product,
     ProductCategory,
@@ -16,38 +12,36 @@ from stock.models import (
 )
 
 
-class ProductTypeFactory(DjangoModelFactory):
+class ProductTypeFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = ProductType
 
     @factory.sequence
     def slug(n):
-        #slug = slugify(factory.Sequence(lambda n: 'type_{:02d}'.format(n)))
         return 'type_{:02d}'.format(n)
 
 
-class ProductCategoryFactory(DjangoModelFactory):
+class ProductCategoryFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = ProductCategory
 
-    product_type = SubFactory(ProductTypeFactory)
+    product_type = factory.SubFactory(ProductTypeFactory)
+
     @factory.sequence
     def slug(n):
-        #slug = slugify(factory.Sequence(lambda n: 'category_{:02d}'.format(n)))
         return 'category_{:02d}'.format(n)
 
 
-class ProductFactory(DjangoModelFactory):
+class ProductFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Product
 
-    category = SubFactory(ProductCategoryFactory)
-    price = FuzzyDecimal(10.00, 100.00)
+    category = factory.SubFactory(ProductCategoryFactory)
+    price = factory.fuzzy.FuzzyDecimal(10.00, 100.00)
 
     @factory.sequence
     def slug(n):
-        #slug = slugify(factory.Sequence(lambda n: 'product_{:02d}'.format(n)))
         return 'product_{:02d}'.format(n)
