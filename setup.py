552e37238664f227242d8eb9832868deb8c0496f
Patrick Kimber <code@pkimber.net>
2014-07-17 21:09:39 +0100

    version 0.1.09

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/stock/*.*',
         ],
     },
-    version='0.1.08',
+    version='0.1.09',
     description='stock',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
